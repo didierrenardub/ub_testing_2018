28d156a18da4d558221edae1283e0cfce5ee40c9
Didier Renard <didier.renard@comunidad.ub.edu.ar>
2018-03-08 22:37:11 -0300

    + Agregadas clases para puntuacion de alumnos

--- src/ar/edu/ub/testing2018/CMain.java
+++ src/ar/edu/ub/testing2018/CMain.java
@@ -3,14 +3,60 @@ package ar.edu.ub.testing2018;
 import ar.edu.ub.testing2018.ConsoleWarriors.CPlayer;
 import ar.edu.ub.testing2018.ConsoleWarriors.CTournament;
 import ar.edu.ub.testing2018.ConsoleWarriors.CWarriorBot;
-import ar.edu.ub.testing2018.Flipboard.CBoard;
-import ar.edu.ub.testing2018.Flipboard.CCard;
 import ar.edu.ub.testing2018.Flipboard.CFlipBot;
+import ar.edu.ub.testing2018.Students.CStudentStatistics;
+import ar.edu.ub.testing2018.Students.IStudent;
+
+import java.util.Scanner;
 
 public class CMain
 {
     private CMain()
     {
+        this.m_stats = new CStudentStatistics();
+    }
+    
+    private CStudentStatistics stats()
+    {
+        return m_stats;
+    }
+    
+    private void studentsGrading()
+    {
+        System.out.println("Average grades and points:");
+        for(IStudent s : stats().students())
+        {
+            System.out.println("\t" + s.name() + " (" + s.points() + "/" + s.earnedPoints() + "): " + s.gradesAverage());
+        }
+    }
+    
+    private int userInput(int min, int max)
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int input = min - 1;
+        
+        while(input < min || input > max)
+        {
+            try
+            {
+                input = s.nextInt();
+            }
+            catch(Exception e)
+            {
+                s.nextLine();
+            }
+            finally
+            {
+                
+                if(input < min || input > max)
+                {
+                    System.out.println("Invalid input, try again:");
+                }
+            }
+        }
+        
+        return input;
     }
 
     private void consoleWarriorsTournament()
@@ -32,7 +78,28 @@ public class CMain
     public static void main(String[] args)
     {
         CMain app = new CMain();
-        app.consoleWarriorsTournament();
-        //app.flipboardTournament();
+    
+        System.out.println("Choose an action:\n1 - Run exercises\n2 - Students grading\n3 - Console Warriors tournament\n4 - Flipboard tournament");
+    
+        switch(app.userInput(1, 4))
+        {
+            case 1:
+                app.stats().runExercises();
+            break;
+            
+            case 2:
+                app.studentsGrading();
+            break;
+        
+            case 3:
+                app.consoleWarriorsTournament();
+            break;
+            
+            case 4:
+                app.flipboardTournament();
+            break;
+        }
     }
+    
+    private CStudentStatistics m_stats;
 }
